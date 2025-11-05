cab83b8c3083cc6a5932a434e78c48d35478dd05
Bapt <baptiste.tron05@gmail.com>
2025-11-05 17:46:13 +0100

    scrapping simplyscripts test

--- scrapping_container/scrapping/main.py
+++ scrapping_container/scrapping/main.py
@@ -1,2 +1,4 @@
 # scrapping scripts execution
-exec(open("scrapping_container/scrapping/scrapping.py").read())
+#exec(open("scrapping_container/scrapping/scrapping.py").read())
+# scrapping scripts execution
+exec(open("scrapping_container/scrapping/scrapping_simplyscripts.py").read())
